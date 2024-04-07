9aec454019e6a926f8f25c68b08866b9cb8151e6
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-07 16:47:33 +0530

    FEAT : user-login

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,70 @@
+import connectToDB from "@/db/connectToDB";
+import ApiResponse from "@/helpers/ApiResponse";
+import validateEmail from "@/helpers/validateEmail";
+import bcrypt from "bcryptjs";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import conf from "@/conf/conf";
+
+connectToDB();
+
+export async function POST(request: NextRequest) {
+    try {
+        const formData = await request.json();
+        const { email, password } = formData;
+
+        // STEP 1 : validate the data at the backend also
+        if ([email, password].some((field) => !field || field?.trim().length === 0)) {
+            return NextResponse.json(new ApiResponse(false, 400, {}, "Some data is missing."));
+        }
+
+        // Step 1.1 Validate email by regex
+        const isValidEmail = validateEmail(email);
+
+        if (!isValidEmail) {
+            return NextResponse.json(new ApiResponse(false, 400, {}, "Invalid email address."));
+        }
+
+        // Step 2 : We need to find the user and check the password
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return NextResponse.json(new ApiResponse(false, 400, {}, "No user with this email address found."));
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return NextResponse.json(new ApiResponse(false, 400, {}, "Incorrect Password."));
+        }
+
+
+        // Step 3 : If the user is verified then only allow login
+        if (!user.isVerified) {
+            return NextResponse.json(new ApiResponse(false, 400, {}, "Kindly verify your account first and then try again."));
+
+        }
+
+        // Step 4 : Create a cookie to send to user
+        const cookieToken = jwt.sign({
+            _id: user._id
+        }, conf.tokenSecret, {
+            expiresIn: '1d'
+        });
+
+        // Step 5 : Send a cookie
+        const response = NextResponse.json(new ApiResponse(true, 200, user, "User successfully Logged In..."));
+
+        response.cookies.set("token", cookieToken, {
+            httpOnly: true,
+            secure: true
+        })
+
+        return response;
+
+    } catch (error: any) {
+        console.log(error.message);
+        return NextResponse.json(new ApiResponse(false, 500, {}, `Internal Server Error.`));
+    }
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { MdLogin } from "react-icons/md";
+
+import { FaGoogle } from "react-icons/fa";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [formData, setFormData] = useState<LoginFormData>({
+    email: "",
+    password: "",
+  });
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleUserLogin = async () => {
+    try {
+      setIsDisabled(true);
+      // Validate data in all the fields
+      if (
+        formData.email.trim().length > 0 &&
+        formData.password.trim().length > 0
+      ) {
+        const response = await axios.post("/api/users/login", formData);
+
+        if (response.data.success) {
+          toast.success(response.data.message);
+          setFormData({
+            email: "",
+            password: "",
+          });
+          setTimeout(() => {
+            router.push("/");
+          }, 1000);
+        } else {
+          toast.error(response.data.message);
+        }
+      } else {
+        toast.error("Please provide data in all the fields...");
+      }
+
+      setIsDisabled(false);
+    } catch (error: any) {
+      setIsDisabled(false);
+      console.log(error.message);
+      toast.error(
+        "Something went wrong while creating your account. Please try again."
+      );
+    }
+  };
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="yourname@example.com"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                href="/forgotpassword"
+                className="ml-auto inline-block text-sm underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="********"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              required
+            />
+          </div>
+          <Button
+            type="submit"
+            className="w-full flex flex-row items-center justify-center gap-x-2"
+            onClick={handleUserLogin}
+            disabled={isDisabled}
+          >
+            <MdLogin className="h-5 w-5" /> Login
+          </Button>
+          <hr className="my-1" />
+          <Button
+            variant="outline"
+            className="w-full flex flex-row items-center justify-center gap-x-2"
+            disabled={isDisabled}
+          >
+            <FaGoogle className="h-4 w-4" />
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="/signup" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
