b1597457d31f0ddd38425eaf4912fa8f94bd3d1a
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-23 10:26:41 +0530

    ADD : middleware-configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,33 @@
-export { auth as middleware } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
+
+export default auth(async (request: NextRequest) => {
+    const session = await auth();
+    console.log(session);
+
+    const path = request.nextUrl.pathname;
+
+    const isPublicPath = path === "/" || path === "/login" || path === "/signup";
+
+    if (session && isPublicPath) {
+        return NextResponse.redirect(new URL("/homepage", request.nextUrl));
+    }
+
+    if (!session && !isPublicPath) {
+        return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+    matcher: [
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+        "/about",
+        "/contact",
+        "/faqs",
+        "/privacypolicy",
+        "/how-to-use",
+        "/resetpassword",
+        "/changepassword",
+    ],
+};
