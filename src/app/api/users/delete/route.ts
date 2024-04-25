a18a80394cfc8efb7200b33d8733657d033c0d05
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-25 12:32:59 +0530

    ADD : delete-user-account-route-wip

---
+++ src/app/api/users/delete/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import novu from "@/helpers/novu";
+import ApiResponse from "@/helpers/ApiResponse";
+
+export async function DELETE(request: NextRequest) {
+    try {
+        // await novu.subscribers.delete("");
+
+        // TODO
+
+        return NextResponse.json(
+            new ApiResponse(true, 200, {}, `User deleted successfully.`)
+        );
+    } catch (error: any) {
+        console.error(`Error while deleting the user : ERROR : ${error}`);
+        return NextResponse.json(
+            new ApiResponse(
+                false,
+                500,
+                {},
+                `Error while deleting the user : ERROR : ${error}`
+            )
+        );
+    }
+}
