1d3d46529cf1fb3c2b4dac902421440353e0ac6e
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-30 18:52:55 +0530

    DEBUG : middleware-fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import conf from "./conf/conf";
 
-
 export async function middleware(request: NextRequest) {
-  const sessionToken = request.cookies.get(conf.sessionTokenName || conf.secureSessionTokenName)?.value || null;
+  const sessionToken =
+    request.cookies.get(conf.sessionTokenName)?.value || null;
+  const secureSessionToken =
+    request.cookies.get(conf.secureSessionTokenName)?.value || null;
 
-  console.log(`COOKIES : ${request.cookies.getAll()}`);
-
-  console.log(`Session Token : ${sessionToken}`);
+  console.log(
+    `Session Token : ${request.cookies.get("authjs.session-token")?.value}`
+  );
+  console.log(
+    `SECURE TOKEN : ${request.cookies.get("__Secure-authjs.session-token")?.value
+    }`
+  );
 
   const path = request.nextUrl.pathname;
 
   // If the user is not logged in
-  if (!sessionToken) {
+  if (!sessionToken || !secureSessionToken) {
     if (
       path === "/homepage" ||
       path === "/myaccount/profile" ||
@@ -24,7 +30,7 @@ export async function middleware(request: NextRequest) {
   }
 
   // If the user is logged in
-  if (sessionToken) {
+  if (sessionToken || secureSessionToken) {
     if (path === "/" || path === "/login" || path === "/signup") {
       return NextResponse.redirect(new URL("/homepage", request.nextUrl));
     }
