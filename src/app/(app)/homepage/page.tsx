df1101b9d697579667eb3821e769a938989ad1e8
mukulpadwal <mukulpadwal.me@gmail.com>
2024-04-30 11:03:54 +0530

    ADD : verified-badge-username

--- src/app/(app)/homepage/page.tsx
+++ src/app/(app)/homepage/page.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import AddRevisionEntry from "@/components/AddRevisionEntry";
 import ScheduleTable from "@/components/ScheduleTable";
 import SearchBar from "@/components/SearchBar";
+import { BadgeCheck } from "lucide-react";
 
 export default function HomePage() {
   const { data: session } = useSession();
@@ -11,8 +12,15 @@ export default function HomePage() {
   return (
     <div className="relative w-full h-screen flex flex-col justify-normal items-center gap-y-4">
       <div className="w-full flex flex-col md:flex-row justify-center md:justify-between items-center gap-y-4 md:gap-y-0 p-2 sm:my-4">
-        <h1 className="text-center sm:text-left text-3xl">
-          Welcome <strong>{session?.user?.username}</strong>
+        <h1 className="text-center sm:text-left text-3xl flex flex-row items-center justify-center space-x-2">
+          <div>
+            Welcome <strong>{session?.user.username} </strong>
+          </div>
+          <div title="Verified Badge">
+            {session?.user.isVerified && (
+              <BadgeCheck className="text-green-500" />
+            )}
+          </div>
         </h1>
         <SearchBar />
       </div>
