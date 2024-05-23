7ca7ffdfe9ddbde171dfd6f10c9f60f8d8361bbb
mukulpadwal <mukulpadwal.me@gmail.com>
2024-05-23 17:05:02 +0530

    FEAT : google-analytics

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,3 +16,4 @@ export const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
